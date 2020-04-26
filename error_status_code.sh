2ddb19fc0ec6edbd5bbf8924c9da9646a4faa29c
Theophile Leroy <perso@tleroy.me>
2020-04-26 23:48:33 +0200

    new error script

--- error_status_code.sh
+++ error_status_code.sh
@@ -8,3 +8,12 @@
 # Usage: ./error_status_code.sh
 ###########################################
 
+# Run cmd that should always SUCCEED
+ls > /dev/null
+ls_ret=$?
+
+# Run cmd that should always FAIL
+mkdir /home/ 2>/dev/null
+mkdir_ret=$?
+
+printf "'ls' returned ${ls_ret}, while 'mkdir /home/' retruned ${mkdir_ret}\n"
