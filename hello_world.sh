a5d6abd02dabf622b33c39d5613a0a3916499485
Theophile Leroy <perso@tleroy.me>
2020-04-26 16:00:11 +0200

    Initial commit

---
+++ hello_world.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###########################################
+# Author: Theophile leroy <pro@tleroy.me>
+# Version: v1.0.0
+# Date: 2020-04-26
+# Description: Basic Hello World Program
+# Usage: ./hello_world.sh
+###########################################
+
+# Print text to terminal
+echo "Hello World!"
+
