62f234ee71d0a5689a5c57797fdfb1407cd19aca
Theophile Leroy <perso@tleroy.me>
2020-04-27 00:04:12 +0200

    Functional testing

--- error_status_code.sh
+++ error_status_code.sh
@@ -17,3 +17,20 @@ mkdir /home/ 2>/dev/null
 mkdir_ret=$?
 
 printf "'ls' returned ${ls_ret}, while 'mkdir /home/' retruned ${mkdir_ret}\n"
+
+# Return code checking vs Functional testing
+
+mkdir /tmp/temp_dir 2>/dev/null
+mkdir_ret=$?
+
+test -d /tmp/temp_dir
+test_ret=$?
+
+# On first run, both will return 0 (succeed). After first run, mkdir will
+# return an error becasue folder already exist, while test will return 0.
+#
+# In some case we will require to use functional testing isntead of simple
+# return code checking.
+
+printf "mkdir resulted in ${mkdir_ret}, test resulted in ${test_ret}\n"
+
